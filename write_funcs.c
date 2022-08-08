40cdae58e54f5e756ac969d236d8df8800db1a21
Matlo21 <mokomanematlotlo21@gmail.com>
2022-08-08 08:21:04 -0700

    My commit

--- write_funcs.c
+++ write_funcs.c
@@ -9,6 +9,7 @@
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
  */
+
 int _putchar(char c)
 {
 	static char buf[1024];
@@ -32,6 +33,7 @@ int _putchar(char c)
  * @str: pointer to the string to print
  * Return: number of chars written
  */
+
 int _puts(char *str)
 {
 	register int i;
